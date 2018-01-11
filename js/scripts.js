1cc4a0f778ff4d96c5ae91ab8c13fcc36e290e5b
Hannah Lee and Kimberly Huynh <hannah@>
2018-01-11 13:49:39 -0800

    debug and remove images

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-  $("form.form").submit(function(){
-    var genderInput = $("select#gender").val();
-    var vehicleInput = $("select#vehicle").val();
-    var beverageInput = $("select#beverage").val();
+  $("form#quiz").submit(function(event) {
+    console.log("quiz reached");
+
+    var gender = $("select#gender").val();
+    var vehicle = $("select#vehicle").val();
 
 
+    if (gender === "Female" && vehicle === "Bike") {
+      console.log("if statement");
+      $("#dwayne").show();
+      $("p").show();
+    } else {
+      console.log("else has been reached");
+    }
+    event.preventDefault();
   });
-
 });
