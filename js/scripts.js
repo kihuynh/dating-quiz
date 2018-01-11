e616ac80252dbbec5647bb24aa980e760a939c97
Hannah Lee and Kimberly Huynh <hannah@>
2018-01-11 14:33:51 -0800

    debug angelina and || bug

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,23 @@ $(document).ready(function(){
 
     var gender = $("select#gender").val();
     var vehicle = $("select#vehicle").val();
+    var beverage = $("select#beverage").val();
 
-
-    if (gender === "Female" && vehicle === "Bike") {
+    if (gender === "Female" && vehicle === "Bike" && (beverage === "Gatorade" || beverage === "Water")) {
       console.log("if statement");
       $("#dwayne").show();
       $("p").show();
+      console.log("it's dwayne");
+    } else if (gender === "Male" && vehicle === "Car" && beverage === "Sparkling Water") {
+      $("#angelina").show();
+      console.log("where");
+      $("p").show();
     } else {
-      console.log("else has been reached");
+      $("h2").show();
     }
+
+
+
     event.preventDefault();
   });
 });
