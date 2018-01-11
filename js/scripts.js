22a8e703551c94422c5ffed656875505785b0cbc
Hannah Lee and Kimberly Huynh <hannah@>
2018-01-11 11:15:54 -0800

    remove option of soda, insert img html, and identify variables for function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   $("form.form").submit(function(){
-    
-
+    var genderInput = $("select#gender").val();
+    var vehicleInput = $("select#vehicle").val();
+    var beverageInput = $("select#beverage").val();
 
 
   });
